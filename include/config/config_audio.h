6e7c9029cb8451872a3dec3a52fe122be085fb04
gheskett <gheskett@gmail.com>
2022-02-26 02:52:40 -0500

    Pointless memory allocation

--- include/config/config_audio.h
+++ include/config/config_audio.h
@@ -21,7 +21,7 @@
  * Vanilla by default only generally allocates 16 or 20 notes at once. Memory usage is always determined by the largest of the two values here (~7200 bytes per US/JP note).
  */
 #define MAX_SIMULTANEOUS_NOTES_EMULATOR 40
-#define MAX_SIMULTANEOUS_NOTES_CONSOLE 28
+#define MAX_SIMULTANEOUS_NOTES_CONSOLE 24
 
 /**
  * Use a much better implementation of reverb over vanilla's fake echo reverb. Great for caves or eerie levels, as well as just a better audio experience in general.
