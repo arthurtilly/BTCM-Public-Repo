6bf5a302993ec0ca7989048f99a930e5611c12b6
Reonu <danileon95@gmail.com>
2023-01-20 15:36:19 +0100

    Added (emu-only) RCVI hack. (#425)
    * Added emu-only RCVI hack
    
    * newline moment
    
    * Changed the value to *20
    
    hi arthur
    
    * RCVI hack compiler warning
    
    * Revise BETTER_REVERB comments to better reflect RCVI hack's existence
    
    * Fix potentially confusing comment
    
    * More BETTER_REVERB typos :peterVOID:
    
    * Address requested PR changes for RCVI Hack
    
    Co-authored-by: gheskett <gheskett@gmail.com>

--- src/game/main.h
+++ src/game/main.h
@@ -89,5 +89,6 @@ extern struct SPTask        *sCurrentDisplaySPTask;
 void set_vblank_handler(s32 index, struct VblankHandler *handler, OSMesgQueue *queue, OSMesg *msg);
 void dispatch_audio_sptask(struct SPTask *spTask);
 void exec_display_list(struct SPTask *spTask);
+void change_vi(OSViMode *mode, int width, int height);
 
 #endif // MAIN_H
