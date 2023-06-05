1390e97b3e0870d2930c70695d96b8f0af8b655c
mineqwerty <mineqwerty25@gmail.com>
2023-06-04 21:19:51 -0400

    added support for up to 99 red coins in an area

--- src/game/behaviors/red_coin.inc.c
+++ src/game/behaviors/red_coin.inc.c
@@ -78,12 +78,6 @@ void bhv_red_coin_loop(void) {
             // For JP version, play an identical sound for all coins.
             create_sound_spawner(SOUND_GENERAL_RED_COIN);
 #else
-            // On all versions but the JP version, each coin collected plays a higher noise.
-
-            //play_sound(SOUND_MENU_COLLECT_RED_COIN
-                       //+ (((u8) o->parentObj->oHiddenStarTriggerCounter - 1) << 16),
-                       //gGlobalSoundSource);
-            
             if (o->parentObj->oHiddenStarTriggerTotal - o->parentObj->oHiddenStarTriggerCounter > 7) {
                 // Play the first red coin sound until it gets to the final 8
                 play_sound(SOUND_MENU_COLLECT_RED_COIN, gGlobalSoundSource);
@@ -94,8 +88,8 @@ void bhv_red_coin_loop(void) {
                         + (((u8) 7 - (o->parentObj->oHiddenStarTriggerTotal - o->parentObj->oHiddenStarTriggerCounter)) << 16),
                         gGlobalSoundSource);
             }
-        }
 #endif
+        }
 
         if (o->prevObj != NULL) {
             o->prevObj->prevObj = NULL;
