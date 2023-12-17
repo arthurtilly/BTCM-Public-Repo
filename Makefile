rovertronic/Mario-Builder-64

Remove redundant s2d directory and delete unused files/defines (#457)

Makefile
@@ -5,6 +5,8 @@ include util.mk
 # Default target
 default: all
 
+TARGET_STRING := sm64
+
 # Preprocessor definitions
 DEFINES :=
 
@@ -189,14 +191,12 @@ GCC_GRAPH_NODE_OPT_FLAGS = \
 #==============================================================================#
 
 ifeq ($(COMPILER),gcc)
-  NON_MATCHING := 1
   MIPSISET     := -mips3
   OPT_FLAGS           := $(GCC_MAIN_OPT_FLAGS)
   COLLISION_OPT_FLAGS  = $(GCC_COLLISION_OPT_FLAGS)
   MATH_UTIL_OPT_FLAGS  = $(GCC_MATH_UTIL_OPT_FLAGS)
   GRAPH_NODE_OPT_FLAGS = $(GCC_GRAPH_NODE_OPT_FLAGS)
 else ifeq ($(COMPILER),clang)
-  NON_MATCHING := 1
   # clang doesn't support ABI 'o32' for 'mips3'
   MIPSISET     := -mips2
   OPT_FLAGS    := $(DEFAULT_OPT_FLAGS)
@@ -204,24 +204,6 @@ else ifeq ($(COMPILER),clang)
   MATH_UTIL_OPT_FLAGS  = $(DEFAULT_OPT_FLAGS)
   GRAPH_NODE_OPT_FLAGS = $(DEFAULT_OPT_FLAGS)
 endif
-
-
-# NON_MATCHING - whether to build a matching, identical copy of the ROM
-#   1 - enable some alternate, more portable code that does not produce a matching ROM
-#   0 - build a matching ROM
-NON_MATCHING ?= 1
-$(eval $(call validate-option,NON_MATCHING,0 1))
-
-ifeq ($(TARGET_N64),0)
-  NON_MATCHING := 1
-endif
-
-ifeq ($(NON_MATCHING),1)
-  DEFINES += NON_MATCHING=1 AVOID_UB=1
-endif
-
-
-TARGET_STRING := sm64
 
 # UNF - whether to use UNFLoader flashcart library
 #   1 - includes code in ROM
@@ -287,15 +269,6 @@ endif
 
 GZIPVER ?= std
 $(eval $(call validate-option,GZIPVER,std libdef))
-
-# GODDARD - whether to use libgoddard (Mario Head)
-#   1 - includes code in ROM
-#   0 - does not
-GODDARD ?= 0
-$(eval $(call validate-option,GODDARD,0 1))
-ifeq ($(GODDARD),1)
-  DEFINES += GODDARD=1
-endif
 
 # Whether to hide commands or not
 VERBOSE ?= 0
@@ -706,17 +679,17 @@ $(BUILD_DIR)/levels/%/leveldata.bin: $(BUILD_DIR)/levels/%/leveldata.elf
 	$(V)$(EXTRACT_DATA_FOR_MIO) $< $@
 
 ifeq ($(COMPRESS),gzip)
-include gziprules.mk
+include compression/gziprules.mk
 else ifeq ($(COMPRESS),rnc1)
-include rnc1rules.mk
+include compression/rnc1rules.mk
 else ifeq ($(COMPRESS),rnc2)
-include rnc2rules.mk
+include compression/rnc2rules.mk
 else ifeq ($(COMPRESS),yay0)
-include yay0rules.mk
+include compression/yay0rules.mk
 else ifeq ($(COMPRESS),mio0)
-include mio0rules.mk
+include compression/mio0rules.mk
 else ifeq ($(COMPRESS),uncomp)
-include uncomprules.mk
+include compression/uncomprules.mk
 endif
 
 #==============================================================================#
@@ -891,7 +864,7 @@ endif
 $(BUILD_DIR)/$(TARGET).objdump: $(ELF)
 	$(OBJDUMP) -D $< > $@
 
-.PHONY: all clean distclean default diff test load
+.PHONY: all clean distclean default test load
 # with no prerequisites, .SECONDARY causes no intermediate target to be removed
 .SECONDARY:
 

arthurtilly/BTCM-Public-Repo

Makefile
@@ -5,6 +5,8 @@ include util.mk
 # Default target
 default: all
 
+TARGET_STRING := mb64
+
 # Preprocessor definitions
 DEFINES :=
 
@@ -193,14 +195,12 @@ GCC_GRAPH_NODE_OPT_FLAGS = \
 #==============================================================================#
 
 ifeq ($(COMPILER),gcc)
-  NON_MATCHING := 1
   MIPSISET     := -mips3
   OPT_FLAGS           := $(GCC_MAIN_OPT_FLAGS)
   COLLISION_OPT_FLAGS  = $(GCC_COLLISION_OPT_FLAGS)
   MATH_UTIL_OPT_FLAGS  = $(GCC_MATH_UTIL_OPT_FLAGS)
   GRAPH_NODE_OPT_FLAGS = $(GCC_GRAPH_NODE_OPT_FLAGS)
 else ifeq ($(COMPILER),clang)
-  NON_MATCHING := 1
   # clang doesn't support ABI 'o32' for 'mips3'
   MIPSISET     := -mips2
   OPT_FLAGS    := $(DEFAULT_OPT_FLAGS)
@@ -208,24 +208,6 @@ else ifeq ($(COMPILER),clang)
   MATH_UTIL_OPT_FLAGS  = $(DEFAULT_OPT_FLAGS)
   GRAPH_NODE_OPT_FLAGS = $(DEFAULT_OPT_FLAGS)
 endif
-
-
-# NON_MATCHING - whether to build a matching, identical copy of the ROM
-#   1 - enable some alternate, more portable code that does not produce a matching ROM
-#   0 - build a matching ROM
-NON_MATCHING ?= 1
-$(eval $(call validate-option,NON_MATCHING,0 1))
-
-ifeq ($(TARGET_N64),0)
-  NON_MATCHING := 1
-endif
-
-ifeq ($(NON_MATCHING),1)
-  DEFINES += NON_MATCHING=1 AVOID_UB=1
-endif
-
-
-TARGET_STRING := mb64
 
 # UNF - whether to use UNFLoader flashcart library
 #   1 - includes code in ROM
@@ -294,15 +276,6 @@ endif
 
 GZIPVER ?= std
 $(eval $(call validate-option,GZIPVER,std libdef))
-
-# GODDARD - whether to use libgoddard (Mario Head)
-#   1 - includes code in ROM
-#   0 - does not
-GODDARD ?= 0
-$(eval $(call validate-option,GODDARD,0 1))
-ifeq ($(GODDARD),1)
-  DEFINES += GODDARD=1
-endif
 
 # Whether to hide commands or not
 VERBOSE ?= 0
@@ -718,17 +691,17 @@ $(BUILD_DIR)/levels/%/leveldata.bin: $(BUILD_DIR)/levels/%/leveldata.elf
 	$(V)$(EXTRACT_DATA_FOR_MIO) $< $@
 
 ifeq ($(COMPRESS),gzip)
-include gziprules.mk
+include compression/gziprules.mk
 else ifeq ($(COMPRESS),rnc1)
-include rnc1rules.mk
+include compression/rnc1rules.mk
 else ifeq ($(COMPRESS),rnc2)
-include rnc2rules.mk
+include compression/rnc2rules.mk
 else ifeq ($(COMPRESS),yay0)
-include yay0rules.mk
+include compression/yay0rules.mk
 else ifeq ($(COMPRESS),mio0)
-include mio0rules.mk
+include compression/mio0rules.mk
 else ifeq ($(COMPRESS),uncomp)
-include uncomprules.mk
+include compression/uncomprules.mk
 endif
 
 #==============================================================================#
@@ -911,7 +884,7 @@ endif
 $(BUILD_DIR)/$(TARGET).objdump: $(ELF)
 	$(OBJDUMP) -D $< > $@
 
-.PHONY: all clean distclean default diff test load
+.PHONY: all clean distclean default test load
 # with no prerequisites, .SECONDARY causes no intermediate target to be removed
 .SECONDARY:
 